d8c9b80278a8743457c59c42054f1c84bed4413a
Kris Burke <kris.burke611@gmail.com>
2016-01-19 18:19:23 -0500

    added expandyDancer class

---
+++ src/expandyDancer.js
@@ -0,0 +1,19 @@
+var ExpandyDancer = function(top, left, timeBetweenSteps){
+  this.big = false;
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass("expandyDancer");
+};
+
+ExpandyDancer.prototype = Object.create(Dancer.prototype);
+ExpandyDancer.prototype.constructor = ExpandyDancer;
+ExpandyDancer.prototype.step = function(timeBetweenSteps) {
+  Dancer.prototype.step.call(this, timeBetweenSteps);
+  console.log(this);
+  if (this.big) {
+    this.$node.animate({"border-width":"-=40px", "border-radius":"-=40px"}, "slow");
+    this.big = false;
+  } else {
+    this.$node.animate({"border-width":"+=40px", "border-radius":"+=40px"}, "slow");
+    this.big = true;
+  }
+};
